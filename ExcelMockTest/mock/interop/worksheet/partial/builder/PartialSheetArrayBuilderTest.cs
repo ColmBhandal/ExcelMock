d351c1c1b79507f67a9a5ce2b316810c9ace0af3
cbhandal <cbhandal@guidewire.com>
2022-02-02 16:14:41 +0000

    1st test case for happy path

--- ExcelMockTest/mock/interop/worksheet/partial/builder/PartialSheetArrayBuilderTest.cs
+++ ExcelMockTest/mock/interop/worksheet/partial/builder/PartialSheetArrayBuilderTest.cs
@@ -1,4 +1,5 @@
-﻿using CsharpExtras.Compare;
+﻿using CsharpExtras.Api;
+using CsharpExtras.Compare;
 using CsharpExtras.Map.Sparse.TwoDimensional;
 using ExcelMock.mock.interop.worksheet.partial.builder;
 using ExcelMock.mock.interop.worksheet.partial.data;
@@ -38,10 +39,33 @@ namespace ExcelMockTest.mock.interop.worksheet.partial.builder
             string[,] values, string[,] formulas, ISparseArray2D<ICellData> expectedData)>
             ProviderFor2DArrayFormulasValuesResultExpected()
         {
+            ICsharpExtrasApi csharpExtrasApi = new CsharpExtrasApi();
             return new List<(int valueRow, int valueColumn, int formulaRow, int formulaColumn,
             string[,] values, string[,] formulas, ISparseArray2D<ICellData> expectedData)>()
             {
-
+                (
+                    1, 1, 3, 3, 
+                    new string[,]
+                    {
+                        { "V_00", "V_01" },
+                        { "V_01", "V_11" }
+                    },
+                    new string[,]
+                    {
+                        { "F_00", "F_01" },
+                        { "F_01", "F_11" }
+                    },
+                    csharpExtrasApi.NewSparseArray2DBuilder<ICellData>(EmptyCellDataImpl.Instance)
+                        .WithValue(new MutableCellDataImpl("V_00", ""), 1, 1)
+                        .WithValue(new MutableCellDataImpl("V_01", ""), 1, 2)
+                        .WithValue(new MutableCellDataImpl("V_10", ""), 2, 1)
+                        .WithValue(new MutableCellDataImpl("V_11", ""), 2, 2)
+                        .WithValue(new MutableCellDataImpl("", "F_00"), 3, 3)
+                        .WithValue(new MutableCellDataImpl("", "F_01"), 3, 4)
+                        .WithValue(new MutableCellDataImpl("", "F_10"), 4, 3)
+                        .WithValue(new MutableCellDataImpl("", "F_11"), 4, 4)
+                        .Build()
+                )
             };
         }
     }
